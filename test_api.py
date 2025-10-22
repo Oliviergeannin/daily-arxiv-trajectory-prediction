8cb9a112410e0cca56256a6406d6a1ef439a1ba3
Oliviergeannin <2393687492@qq.com>
2025-10-22 23:01:16 +0800

    revised

--- test_api.py
+++ test_api.py
@@ -4,6 +4,8 @@ import openai
 import requests
 
 load_dotenv()
+# print("读取到的令牌:", os.getenv("MODELSCOPE_ACCESS_TOKEN"))
+
 
 client = openai.OpenAI(
     api_key=os.getenv("MODELSCOPE_ACCESS_TOKEN"),
